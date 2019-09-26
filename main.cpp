a329982a9fe1fdb4b60af8adefbec6a5e99f85b2
CSWMandrews <wmandrews@alaska.edu>
2019-09-25 19:07:10 -0800

    Enabled input of Height variable and successfully printed requested box.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,33 @@ using std::ofstream;
 
 int main() {
     cout << "Hello, World!" << endl;
+
     cout << "How wide of a box do you want?" << endl;
 
     int x;
-
     cin >> x;
-
     cout << "Width: " << x << endl;
+
+    cout << "How tall of a box do you want?" << endl;
+
+    int y;
+    cin >> y;
+    cout << "Height: " << y << endl;
+
+    int xX=0;
+    int yY=0;
+    while (yY<y)
+    {
+        xX=0;
+        while (xX < x) {
+            cout << "X";
+            xX++;
+        }
+        cout << endl;
+        yY++;
+    }
+
+
 
 
     return 0;
