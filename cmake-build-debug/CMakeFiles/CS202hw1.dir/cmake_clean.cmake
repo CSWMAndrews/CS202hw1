2c11fcd434644994657eb0c399f6d046a8536129
CSWMandrews <wmandrews@alaska.edu>
2019-09-25 18:54:55 -0800

    Enabled input of Width variable

---
+++ cmake-build-debug/CMakeFiles/CS202hw1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS202hw1.dir/main.cpp.obj"
+  "CS202hw1.pdb"
+  "CS202hw1.exe"
+  "CS202hw1.exe.manifest"
+  "libCS202hw1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS202hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+using std::getline;
+#include <sstream>
+using std::istringstream;
+#include <fstream>
+using std::ofstream;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "How wide of a box do you want?" << endl;
+
+    int x;
+
+    cin >> x;
+
+    cout << "Width: " << x << endl;
+
+
+    return 0;
+
+
+}
